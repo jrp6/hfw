098e47993b437472533e068c9c6377b5bb33aedb
Juho Rinta-Paavola <juho.s.rp@gmail.com>
2018-01-13 22:45:44 +0200

    Add twinkling stylish stars

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d');
 const resize = () => {
   canvas.width = canvas.clientWidth;
   canvas.height = canvas.clientHeight;
-}
+};
 window.addEventListener('resize', resize);
 resize();
 
@@ -21,6 +21,33 @@ const drawStar = (x, y, r) => {
   ctx.lineTo(x - smallR, y - smallR);
   ctx.closePath();
   ctx.fill();
+};
+
+let stars = [];
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
+const getRandomStarParams = () => [
+  getRandomInt(canvas.width),
+  getRandomInt(canvas.height),
+  getRandomInt(13 - 4) + 4, // radius
+];
+
+// How many stars per pixel
+const starDensity = 0.0002;
+
+const nStars = starDensity * canvas.width * canvas.height;
+for (let i = 0; i < nStars; i += 1) {
+  stars.push(getRandomStarParams());
 }
 
-drawStar(100, 100, 12);
+setInterval(() => {
+  const toRemove = getRandomInt(stars.length);
+  stars.splice(toRemove, 1);
+  stars.push(getRandomStarParams());
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'red';
+  stars.forEach(([x, y, r]) => drawStar(x, y, r));
+}, 350);
